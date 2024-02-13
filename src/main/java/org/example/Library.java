2863446caa31e11ca1aede29fd62e139bca9ed1c
Никита Кремнёв <nkremnev3003@gmail.com>
2024-02-13 17:27:22 +0300

    Добавлен простой класс "Library", который моделирует работу библиотеки.

---
+++ src/main/java/org/example/Library.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+}
