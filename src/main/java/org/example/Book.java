615d1a98bdaf0c8a8beafabfc9303304eef8c1e0
Никита Кремнёв <nkremnev3003@gmail.com>
2024-02-13 17:23:03 +0300

    Добавлен простой класс "Book", который моделирует книгу и содержит в себе поля названия, автора и маркер доступности. Добавлены геттеры и сеттеры.

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Book {
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book(String title, String author, boolean available) {
+        this.title = title;
+        this.author = author;
+        this.available = available;
+    }
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+}
